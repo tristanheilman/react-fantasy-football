21c979f5d38db1a8e85d811528b5500a6d0a37d9
Treeeest <tristanheilman@outlook.com>
2020-10-06 18:03:58 -0400

    push sql changes; updated sample component to display NFL teams in our database

--- src/components/sample-component/sample-component.jsx
+++ src/components/sample-component/sample-component.jsx
@@ -1,16 +1,28 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 
-function getUsers() {
-  fetch(`/api/users`)
-  .then((response) => response.json())
-  .then(users => console.log(users));
-}
+
+function getUsers(setTeams) {
+    fetch(`/api/users`)
+    .then((response) => response.json())
+    .then(result => setTeams(result.data));
+};
 
 export function SampleComponent() {
-  return (
-    <div>
-      This is a sample component
-      {getUsers()}
-    </div>
-  );
+    const [teams, setTeams] = React.useState([]);
+
+    useEffect(() => {
+        getUsers(setTeams);
+    }, [])
+
+    console.log("TEAMS: ", teams);
+
+    return (
+        <div>
+        This is a sample component
+
+        <ol>
+        {teams.length > 0 ? teams.map((item, index) => <li key={index}>{item.team}</li>) : null}
+        </ol>
+        </div>
+    );
 }
--- src/server/server.js
+++ src/server/server.js
@@ -1,27 +1,67 @@
 const express = require('express');
 const mysql = require('mysql');
+const { Connection, Request } = require("tedious");
 
 const app = express();
 const port = 8000;
-const table ='users';
 
-const pool = mysql.createPool({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USER,
-  password: process.env.MYSQL_PWD,
-  database: process.env.MYSQL_DB,
-});
+// Create connection to database
+const config = {
+  authentication: {
+    options: {
+      userName: "azureuser", // update me
+      password: "Password1234" // update me
+    },
+    type: "default"
+  },
+  server: "sqlservermidterm.database.windows.net", // update me
+  options: {
+    database: "nflmidterm", //update me
+    encrypt: true
+  }
+};
 
 app.listen(port, () => {
   console.log(`App server now listening to port ${port}`);
 });
 
 app.get('/api/users', (req, res) => {
-  pool.query(`select * from ${table}`, (err, rows) => {
-    if (err) {
-      res.send(err);
-    } else {
-      res.send(rows);
-    }
-  });
+    const connection = new Connection(config);
+
+    // Attempt to connect and execute queries if connection goes through
+    connection.on("connect", err => {
+      if (err) {
+        console.error(err.message);
+      } else {
+          queryDatabase(connection, res);
+      }
+    });
 });
+
+function queryDatabase(conn, result) {
+    console.log("Reading rows from the Table...");
+
+    // Read all rows from table
+    let data = [];
+    const request = new Request(
+        `SELECT * FROM [dbo].[Teams]`,
+        (err, rowCount, rows) => {
+            if (err) {
+                result.send({ status: 500, data: null, message: "internal server error."});
+            } else {
+                result.send({ status: 200, data: data, message: "OK"});
+            }
+        }
+    );
+
+    request.on('row', function(row){
+        data.push({
+            id: row[0].value,
+            team: row[1].value,
+            LastYearPoints: row[2].value,
+            ThisYearPoints: row[3].value
+        });
+    })
+
+    conn.execSql(request);
+}
